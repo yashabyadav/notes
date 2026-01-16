7e9dac0ec73bf604bd5e06a730811dc247778a02
Yashab Yadav <yay223@lehigh.edu>
2026-01-15 20:29:15 -0800

    changed theme to flexoki for now, might change later, forgot to add this file with the last commit

--- quartz.config.ts
+++ quartz.config.ts
@@ -26,7 +26,31 @@ const config: QuartzConfig = {
         body: "Source Sans Pro",
         code: "JetBrains Mono",
       },
-    colors: palettes[themeKeyword],
+      // Dummy colors - actual theme colors are defined in quartz/styles/themes/_index.scss
+      colors: {
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
     },
   },
   plugins: {
